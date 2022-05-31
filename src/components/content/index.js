a6652b8e8649347f5079070c715acfd2d65f3d73
Lucas Cardoso Lima da Silva <lucas.cardoso@LINK-DATA.ASI>
2022-05-31 13:59:02 -0300

    buttons edit

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,8 +1,25 @@
 import { ContentStyle } from "./style";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
-
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import livro from "../../assets/fotos/livro.jpg"
 const searchIcon = <FontAwesomeIcon icon={faSearch} />
+const arrowLeft = <FontAwesomeIcon icon={faChevronLeft} className="icon"/>
+const arrowRight = <FontAwesomeIcon icon={faChevronRight} className="icon"/>
+
+const ProjectItem = (props) => {
+  return (
+    <div className= {props.class}>
+      <div className = "square">
+        <button className="left">{arrowLeft}</button>
+        <img src={livro} alt="Minha Figura" />
+        <button className="right">{arrowRight}</button>
+      </div>
+      <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
+    </div>
+  )
+} 
 
 function Content() {
     return (
@@ -12,35 +29,11 @@ function Content() {
           </div>
         </header>
         <div className="content container">
-          <div className="cardContent">
-            <div className = "square"></div>
-            <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
-          </div>
-
-          <div className="cardReverse">
-            <div className = "square"></div>
-            <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
-          </div>
-
-          <div className="cardContent">
-            <div className = "square"></div>
-            <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
-          </div>
-
-          <div className="cardReverse">
-            <div className = "square"></div>
-            <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
-          </div>
-
-          <div className="cardContent">
-            <div className = "square"></div>
-            <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
-          </div>
-
-          <div className="cardReverse">
-            <div className = "square"></div>
-            <p>Considerando que temos bons administradores de rede, a implementação do código agrega valor ao serviço prestado dos índices pretendidos. Assim mesmo, a complexidade computacional cumpre um papel essencial na implantação da autenticidade das informações.</p>
-          </div>
+          <ProjectItem class="cardContent"/>
+          <ProjectItem class="cardReverse"/>
+          <ProjectItem class="cardContent"/>
+          <ProjectItem class="cardReverse"/>
+          <ProjectItem class="cardContent"/>
         </div>
       </ContentStyle>
     );
--- src/components/content/style.js
+++ src/components/content/style.js
@@ -19,17 +19,46 @@ export const ContentStyle = styled.div`
         width: 100%;
     }
     .square{
+        position: relative;
         width: 240px;
         height: 180px;
         background-color: black;
-        transition: 0.4s;
+        transition: 0.7s;
         z-index: 10;
         background: url(${asi});
         background-size: cover;
-        cursor: pointer;
+        img{
+            width: 100%;
+            height: auto;
+            background-size: cover;
+        }
+        .left{
+            top: calc(50% - 13px);
+            left: 2px
+        }
+        .right{
+            top: calc(50% - 13px);
+            right: 2px
+        }
+        button{
+            position: absolute;
+            display: inline-block;
+            height: 26px;
+            padding: 5px;
+            background-color: transparent;
+            .icon{
+                font-size: 24px;
+                color: rgba(0,0,0,0.25);
+                transition: 0.7s;
+                
+            }
+        }
         :hover{
             width: 480px;
             height: 360px;
+            .icon{
+                color: black;
+            }
         }
     }
     .content{
